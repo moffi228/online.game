48bf61c87245a305a23096f0485fcfff273650f0
moffi228 <marchyk26072012@gmail.com>
2025-10-25 12:01:38 +0300

    Update level_1.py

--- level_1.py
+++ level_1.py
@@ -1,10 +1,11 @@
 from pygame import*
 from random import randint
+from math import hypot
 init()
-window=display.set_mode((800,800))
+window=display.set_mode((800,600))
 display.set_caption('Agar.io')
 
-window.fill((255, 165, 0))
+window.fill((205, 227, 233))
 clock=time.Clock()
 
 my_player=[0,0,20]
@@ -16,15 +17,33 @@ class Food():
         self.y=y
         self.radius=r
         self.color=c
+    def check_collision(self,player_x,player_y,player_r):
+        dx=self.x - player_x
+        dy=self.y - player_y
+        return hypot(dx, dy) <= self.radius + player_r
 
-eats=[Food(randint(0,800),randint(0,600),10,(randint(0,255),randint(0,255),randint(0,255)))]
+eats=[]
+for i in range(30):
+    eats.append(Food(randint(0,800),randint(0,600),10,(randint(0,255),randint(0,255),randint(0,255))))
 
+running=True
+while running:
+    window.fill((205, 227, 233))
+    scale=max(0.3, min(50 / my_player[2], 1.5))
 
-while True:
+    to_remove=[]
+
     for e in event.get():
         if e.type==QUIT:
-            False
-    draw.circle(window,(255, 0, 0), (500,500), my_player[2])
+            quit()
+    window.fill((205, 227, 233))
+    for food in eats:
+        draw.circle(window,food.color,(food.x,food.y),food.radius)
+        if food.check_collision(my_player[0],my_player[1],my_player[2]):
+            my_player[2] += int(food.radius * 0.2)
+            to_remove.append(food)
+            eats.remove(food)
+    draw.circle(window,(211, 105, 233),(my_player[0],my_player[1]), my_player[2])
     keys = key.get_pressed()
     if keys[K_w]:my_player[1] -= 15
     if keys[K_s]:my_player[1] += 15
