d3e6afd70f1a92ae875ce3b6d4aecc593fc75390
moffi228 <marchyk26072012@gmail.com>
2026-01-08 21:44:11 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,18 +1,8 @@
-from customtkinter import*
-window=CTk()
-window.geometry("400x400")
-import os
-def easy_level():
-    os.system("python livl1.py")
+WIDTH = 800
+HEIGHT = 600
+FPS = 60
+TITLE = "Online Game"
 
-easy=CTkButton(window, command=easy_level, width=200,height=100, text="Легкий", border_color="black", border_width=2, hover_color="green")
-easy.place(x=120, y=30)
-def medium_level():
-    os.system("python lvel2.py")
-
-medium=CTkButton(window, command=medium_level, width=200, height=100, text="Середній", border_color="black", border_width=2, hover_color="green")
-medium.place(x=120, y=150) 
-
-hard=CTkButton(window, width=200, height=100, text="Тяжкий", border_color="black", border_width=2, hover_color="green")
-hard.place(x=120,y=270)
-window.mainloop()
+LEVEL_1_FILE = "level1.py"
+LEVEL_2_FILE = "level2.py"
+LEVEL_3_FILE = "level3.py"
